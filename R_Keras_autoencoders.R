8c2686da60d5c3e41820ba3f78cfdb8cbb47873f
Arun Kiran Aryasomayajula <arun.aa1983@gmail.com>
2018-02-21 09:07:34 -0600

    Add files via upload

--- R_Keras_autoencoders.R
+++ R_Keras_autoencoders.R
@@ -1,3 +1,6 @@
+## Please refer to the blog post for original problem and explanations
+## https://tensorflow.rstudio.com/blog/keras-fraud-autoencoder.html
+
 library(readr)
 df <- read_csv("~\\Projects\\GNHA_Meetup\\creditcard.csv", col_types = list(Time = col_number()))
 
@@ -88,3 +91,62 @@ loss <- evaluate(model, x = x_test[y_test == 0,], y = x_test[y_test == 0,])
 loss
 
 
+##Tuning the model using cloudml
+
+FLAGS <- flags(
+        flag_string("normalization", "minmax", "One of minmax, zscore"),
+        flag_string("activation", "relu", "One of relu, selu, tanh, sigmoid"),
+        flag_numeric("learning_rate", 0.001, "Optimizer Learning Rate"),
+        flag_integer("hidden_size", 15, "The hidden layer size")
+)
+
+model %>% compile(
+        optimizer = optimizer_adam(lr = FLAGS$learning_rate), 
+        loss = 'mean_squared_error'
+)
+
+library(cloudml)
+cloudml_train("train.R", config = "tuning.yml")
+
+job_collect()
+
+latest_run()$run_dir
+
+ls_runs(order = metric_val_loss, decreasing = FALSE)
+
+model <- load_model_hdf5("runs/cloudml_2018_01_23_221244595-03/model.hdf5", compile = FALSE)
+
+pred_train <- predict(model, x_train)
+mse_train <- apply((x_train - pred_train)^2, 1, sum)
+
+pred_test <- predict(model, x_test)
+mse_test <- apply((x_test - pred_test)^2, 1, sum)
+
+library(Metrics)
+auc(y_train, mse_train)
+auc(y_test, mse_test)
+
+possible_k <- seq(0, 0.5, length.out = 100)
+precision <- sapply(possible_k, function(k) {
+        predicted_class <- as.numeric(mse_test > k)
+        sum(predicted_class == 1 & y_test == 1)/sum(predicted_class)
+})
+
+qplot(possible_k, precision, geom = "line") + labs(x = "Threshold", y = "Precision")
+
+recall <- sapply(possible_k, function(k) {
+        predicted_class <- as.numeric(mse_test > k)
+        sum(predicted_class == 1 & y_test == 1)/sum(y_test)
+})
+qplot(possible_k, recall, geom = "line") + labs(x = "Threshold", y = "Recall")
+
+cost_per_verification <- 1
+
+lost_money <- sapply(possible_k, function(k) {
+        predicted_class <- as.numeric(mse_test > k)
+        sum(cost_per_verification * predicted_class + (predicted_class == 0) * y_test * df_test$Amount) 
+})
+
+qplot(possible_k, lost_money, geom = "line") + labs(x = "Threshold", y = "Lost Money")
+
+possible_k[which.min(lost_money)]
